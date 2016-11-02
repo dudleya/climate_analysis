cfee4a7fbb07afb1a19b9132d2f6edecd59f78f2
yy13g14 <yy13g14@soton.ac.uk>
2016-11-02 15:17:03 +0000

    Added task for developer A

--- rainfall_conversion.R
+++ rainfall_conversion.R
@@ -9,3 +9,18 @@ inches_to_mm <- function(inches){
   mm<-inches * 25.4
   return(mm)
   }
+  
+  #A library to perform rainfall unit conversions
+
+inches_to_mm <-  function(inches){
+  #Convert inches to milimetres.
+  
+  "
+  Arguments:
+  #inches -- the rainfall inches
+  "
+  mm <- inches * 25.4
+  return(mm)
+}
+
+#[TODO: developer A]Add inches to cm conversion function.
